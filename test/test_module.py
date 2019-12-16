fe5ac401f716dc6e5f4f54432dd6486c2f6725f5
JoshuaWu1997 <wujunda1234@icloud.com>
2019-12-17 01:01:35 +0800

    commit on master

--- test/test_module.py
+++ test/test_module.py
@@ -1,10 +1,11 @@
-from numba import cuda
+from numba import cuda, jit, float32
 import numpy as np
+import math
 
 up_lmt = 0.5
 low_lmt = 0.001
-split_num = 10000
-batch_size = 10000
+split_num = 1000
+batch_size = 100
 iter_num = 3000
 test_points = np.array(
     [[r * (up_lmt - low_lmt) / split_num + low_lmt, 0.3] for r in range(split_num)] * batch_size,
@@ -44,9 +45,63 @@ def improved_cpu(x, y):
     return x
 
 
+def numpy_cpu(x, y):
+    x = np.ascontiguousarray(x)
+    y = np.ascontiguousarray(y)
+    z = np.ascontiguousarray([[5.25, 4]] * x.shape[1]).T
+    s = np.ascontiguousarray([[-2, 0.25], [0.5, -2]])
+    for k in range(iter_num):
+        x = np.add(
+            np.multiply(
+                x,
+                np.multiply(
+                    y,
+                    np.add(
+                        z,
+                        np.matmul(
+                            s,
+                            x
+                        )
+                    )
+                )
+            ),
+            x
+        )
+    return x
+
+
+@cuda.jit
+def gpu_iter_2(A, B, C):
+    sA = cuda.shared.array(shape=(2, 1), dtype=float32)
+    sB = cuda.shared.array(shape=(2, 2), dtype=float32)
+    sA[0, 0] = 5.24
+    sA[1, 0] = 4
+    sB[0, 0] = -2
+    sB[1, 0] = 0.5
+    sB[0, 1] = 0.25
+    sB[1, 1] = -2
+    x, y = cuda.grid(2)
+    if x <= A.shape[0] and y <= A.shape[1]:
+        C[x, y] = A[x, y] * B[x, y] * (sA[x, 0] + sB[x, 0] * A[0, y] + sB[x, 1] * A[1, y]) + A[x, y]
+
+
+def gpu_2D(x, y):
+    x = np.ascontiguousarray(x)
+    y = np.ascontiguousarray(y)
+    z = np.ascontiguousarray([[5.25, 4]] * x.shape[1]).T
+    threadsperblock = (2, 512)
+    blockspergrid_x = math.ceil(x.shape[0] + (threadsperblock[0] - 1)) // threadsperblock[0]
+    blockspergrid_y = math.ceil(x[0].shape[0] + (threadsperblock[1] - 1)) // threadsperblock[1]
+    blockspergrid = (blockspergrid_x, blockspergrid_y)
+    for k in range(3000):
+        gpu_iter_2[blockspergrid, threadsperblock](x, y, z)
+        x = z
+    return x
+
+
 def iter_function(x, y):
-    threadsperblock = 512
-    blockspergrid = (x[0].size + (threadsperblock - 1)) // threadsperblock
-    gpu_iter[blockspergrid, threadsperblock](x[0], x[1], y[0], y[1])
-    # x = improved_cpu(x, y)
+    threadsperblock = 1024
+    blockspergrid = math.ceil(x[0].size + (threadsperblock - 1)) // threadsperblock
+    # gpu_iter[blockspergrid, threadsperblock](x[0], x[1], y[0], y[1])
+    x = gpu_2D(x, y)
     return x
